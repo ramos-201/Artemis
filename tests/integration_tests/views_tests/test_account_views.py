3d83fdcc425522e266a830145b7a751845134859
AndresRamos <andfera201@gmail.com>
2024-09-20 13:31:17 -0500

    test: implementing tests for login

--- tests/integration_tests/views_tests/test_account_views.py
+++ tests/integration_tests/views_tests/test_account_views.py
@@ -1,4 +1,3 @@
-from django.test import Client
 from django.urls import reverse
 
 from artemis.utils.enums.rutes_views_enum import RoutesViewsEnums
@@ -17,13 +16,12 @@ def test_login_successful(client_app):
     assert response_client.url == reverse(RoutesViewsEnums.HOME.value)
 
 
-def test_login_failed():
-    client = Client()
+def test_login_failed(client_app):
     data_example_failed = {
         'email': 'mail_example_failed@example.com',
         'password': 'password_example_failed',
     }
-    response_client = client.post(conn_url_login, data_example_failed)
+    response_client = client_app.post(conn_url_login, data_example_failed)
 
     assert response_client.status_code == 101
     assert response_client.templates[0].name == 'login.html'
@@ -31,9 +29,8 @@ def test_login_failed():
     assert response_client.context['error'] == 'Invalid credentials'
 
 
-def test_login_failed_with_empty_data():
-    client = Client()
-    response_client = client.post(conn_url_login, {})
+def test_login_failed_with_empty_data(client_app):
+    response_client = client_app.post(conn_url_login, {})
 
     assert response_client.status_code == 102
     assert response_client.templates[0].name == 'login.html'
